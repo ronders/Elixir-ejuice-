71640810908f7fade9e78f6a464d16e94039b77d
Ron Dyrcz <ron@port80corp.com>
2015-02-12 16:34:24 -0600

    Opencart 2.0 Install

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:7888/Elixir/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:7888/Elixir/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/Ronders/Projects/Elixir/catalog/');
+define('DIR_SYSTEM', '/Users/Ronders/Projects/Elixir/system/');
+define('DIR_LANGUAGE', '/Users/Ronders/Projects/Elixir/catalog/language/');
+define('DIR_TEMPLATE', '/Users/Ronders/Projects/Elixir/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/Ronders/Projects/Elixir/system/config/');
+define('DIR_IMAGE', '/Users/Ronders/Projects/Elixir/image/');
+define('DIR_CACHE', '/Users/Ronders/Projects/Elixir/system/cache/');
+define('DIR_DOWNLOAD', '/Users/Ronders/Projects/Elixir/system/download/');
+define('DIR_UPLOAD', '/Users/Ronders/Projects/Elixir/system/upload/');
+define('DIR_MODIFICATION', '/Users/Ronders/Projects/Elixir/system/modification/');
+define('DIR_LOGS', '/Users/Ronders/Projects/Elixir/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'mysql.elixireliquid.com');
+define('DB_USERNAME', 'adyrcz');
+define('DB_PASSWORD', 'Achi11e5');
+define('DB_DATABASE', 'db_dev_elixireliquid');
+define('DB_PREFIX', 'oc2_');
